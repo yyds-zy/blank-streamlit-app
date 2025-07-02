631c1bf2610e2d9819db030e01e61ac5cc7fe8c3
Zhi Yuan <1311257372@qq.com>
2025-07-02 02:55:09 +0000

    create project

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 我的心晴管家")
